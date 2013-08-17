990d61ada6e536b5d50bdbbbc89078e3b5290879
jbrooks <jonathan.brooks@brmi.com>
2013-08-17 19:37:42 -0400

    Adding project to git.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StockBreakdown::Application.config.secret_key_base = '85e4fcd6c297546d1dfb7f2a0e2ec52fa1a3f346defcfaf5215f2b34419f9697e66051550adf1acc3b83aadae93273abcdf3c49efcaf4e049f8c67620bb22e87'
